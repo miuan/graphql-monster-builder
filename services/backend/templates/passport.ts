a9b06c7893037db611de248951a1342f2fb852c9
Milan Medlik <milan.medlik@gmail.com>
2021-05-05 07:33:17 +0200

    not enable passport for not configured

--- services/backend/templates/passport.ts
+++ services/backend/templates/passport.ts
@@ -50,19 +50,81 @@ export const jwtSetup = (app) => {
     app.use(passport.authenticate(['jwt', 'anonymous']))
 }
 
+const checkIfStrategyConfigured = (config) => !!config.clientID || !!config.clientSecret || !!config.callbackURL
+
+const checkErrorsInConfForStrategy = (config) => {
+    const result = []
+
+    if(!config.clientID){
+        result.push('Client ID is empty')
+    }
+
+    if(!config.clientSecret){
+        result.push('Client Secret is empty')
+    }
+
+    if(!config.callbackURL){
+        result.push('Callback URL is empty')
+    }
+
+    if(config.callbackURL && (!config.callbackURL?.startsWith('http://') || !config.callbackURL?.startsWith('https://'))){
+        result.push(`Callback URL should start with 'http://' or 'https://'`)
+    }
+
+    return result.length ? result : false
+}
+
+
+const setupStrategy = (passport, Strategy, name, config, cb) => {
+    // not configured at all
+    if(!checkIfStrategyConfigured(config)) return false
+
+    // configured with errors
+    const errors = checkErrorsInConfForStrategy(config)
+    if(errors){
+        console.log(`Strategy with name: ${name} have followed errors`, errors)
+        return false
+    }
+    
+    passport.use(name, new Strategy(config, cb))
+    return true
+}
+
+const go = async (ctx) => {
+    console.log('GO', ctx, ctx.req?.user)
+   
+    const userId = ctx.req?.user?.id
+    const user = await userModel.findById(userId)
+    
+    genPasswordAndTokens(user)
+    user.verified = true
+    await user.save()
+    
+    ctx.body = {
+        user:{
+            id: user._id,
+            email: user.email,
+            roles: user.roles
+        },
+        token: user.__token,
+        refreshToken: user.__refreshToken
+    }
+
+    // Successful authentication, redirect home.
+   
+}
 
 export const othersSetup = (authRouter, name=null) => {
     const facebookName = name ? `facebook-${name}`: 'facebook'
     const githubName = name ? `github-${name}`: 'github'
     const googleName = name ? `google-${name}`: 'google'
 
-    passport.use(facebookName, new FacebookStrategy({
+    const facebookConnect = setupStrategy(passport, FacebookStrategy, facebookName, {
         clientID: process.env.FACEBOOK_APP_ID,
         clientSecret: process.env.FACEBOOK_APP_SECRET,
         callbackURL: process.env.FACEBOOK_APP_CALLBACK_URL,
-        profileFields: ["name", "email", "link", "locale", "timezone"],
-
-    }, (accessToken, refreshToken, profile, cb) => {
+        profileFields: ["name", "email", "link", "locale", "timezone"]
+    },  (accessToken, refreshToken, profile, cb) => {
         console.log('Facebook profile', {accessToken, refreshToken, profile})
        userModel.findOne({ __password:  `facebook-id:${profile.id}`}, {_id: 1, roles:1}, (err, existingUser) => {
             if(existingUser) return cb(null, existingUser)
@@ -71,13 +133,14 @@ export const othersSetup = (authRouter, name=null) => {
                 cb(errCreate, createdUser)
             })
         })
-    }))
+    })
 
-    passport.use(githubName, new GitHubStrategy({
+
+    const githubConnect = setupStrategy(passport, GitHubStrategy, githubName, {
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
         callbackURL: process.env.GITHUB_CLIENT_CALLBACK_URL
-    }, (accessToken, refreshToken, profile, cb) => {
+    },  (accessToken, refreshToken, profile, cb) => {
         console.log('GitHub profile', {accessToken, refreshToken, profile})
         userModel.findOne({ __password:  `github-id:${profile.id}`}, {_id: 1, roles:1}, (err, existingUser) => {
             if(existingUser) return cb(null, existingUser)
@@ -86,14 +149,14 @@ export const othersSetup = (authRouter, name=null) => {
                 cb(errCreate, createdUser)
             })
         })
-    }
-    ))
+    })
 
-    passport.use(googleName, new GoogleStrategy({
+
+    const googleConnect = setupStrategy(passport, GitHubStrategy, githubName, {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: process.env.GOOGLE_CLIENT_CALLBACK_URL
-      }, (request, accessToken, refreshToken, profile, done) => {
+    }, (request, accessToken, refreshToken, profile, done) => {
         (accessToken, refreshToken, profile, cb) => {
             console.log('GOOGLE profile', {accessToken, refreshToken, profile})
             userModel.findOne({ __password:  `google-id:${profile.id}`}, {_id: 1, roles:1}, (err, existingUser) => {
@@ -105,45 +168,27 @@ export const othersSetup = (authRouter, name=null) => {
             })
         }
       }
-    ))
-    
+    )
 
-      
-    const go = async (ctx) => {
-        console.log('GO', ctx, ctx.req?.user)
-       
-        const userId = ctx.req?.user?.id
-        const user = await userModel.findById(userId)
-        
-        genPasswordAndTokens(user)
-        user.verified = true
-        await user.save()
-        
-        ctx.body = {
-            user:{
-                id: user._id,
-                email: user.email,
-                roles: user.roles
-            },
-            token: user.__token,
-            refreshToken: user.__refreshToken
-        }
     
-        // Successful authentication, redirect home.
-       
+    if(facebookConnect){
+        authRouter.get('/facebook', passport.authenticate(facebookName));
+        authRouter.get('/facebook/callback', passport.authenticate(facebookName), go);
     }
 
-    authRouter.get('/facebook', passport.authenticate(facebookName));
-    authRouter.get('/facebook/callback', passport.authenticate(facebookName), go);
+    if(githubConnect){
+        authRouter.get('/github', passport.authenticate(githubName, { scope: [ 'user:email' ] }));
+        authRouter.get('/github/callback', passport.authenticate(githubName, { failureRedirect: '/login' }), go);
+    }
 
-    authRouter.get('/github', passport.authenticate(githubName, { scope: [ 'user:email' ] }));
-    authRouter.get('/github/callback', passport.authenticate(githubName, { failureRedirect: '/login' }), go);
-
-    authRouter.get('/google', passport.authenticate(googleName, {scope: [
-        'https://www.googleapis.com/auth/userinfo.profile',
-        'https://www.googleapis.com/auth/userinfo.email'
-    ] }));
-    authRouter.get('/google/callback', passport.authenticate(googleName, { failureRedirect: '/login' }), go);
+    
+    if(googleConnect){
+        authRouter.get('/google', passport.authenticate(googleName, {scope: [
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email'
+        ] }));
+        authRouter.get('/google/callback', passport.authenticate(googleName, { failureRedirect: '/login' }), go);
+    }
 }
 
 export default allSetup
