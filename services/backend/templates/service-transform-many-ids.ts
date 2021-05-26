ab1d287c5ba24d1f0118aa9286922368bf58599e
Milan Medlik <milan.medlik@gmail.com>
2021-05-26 12:12:38 +0200

    add relations by id or object can be together

--- services/backend/templates/service-transform-many-ids.ts
+++ services/backend/templates/service-transform-many-ids.ts
@@ -1,18 +1,21 @@
  // templates/service-transform-many-ids.ts
  // case where data._MEMBER_NAME_ have many ids or many objects
   let _LINKDED_IDS_ = [];
-  if (data._PAYLOAD_NAME_FOR_ID_) {
-    _LINKDED_IDS_ = data._PAYLOAD_NAME_FOR_ID_;
-    data._MEMBER_NAME_ = data._PAYLOAD_NAME_FOR_ID_;
-    delete data._PAYLOAD_NAME_FOR_ID_;
-    
-  } else if (data._PAYLOAD_NAME_FOR_CREATE_) {
+
+  if (data._PAYLOAD_NAME_FOR_CREATE_) {
     const idsOfCreated = []
     for(const createdFrom of data._PAYLOAD_NAME_FOR_CREATE_){
       _SWITCH_OF_ADD_CONNECTED_ID_createdFrom._CONNECTED_MEMBER_NAME_ = _CONNECTED_MEMBER_ID_
       const created = await entry.services['_LOWER_NAME_'].create(createdFrom);
-      idsOfCreated.push(created.id);
+      idsOfCreated.push(created.id)
+      _SWITCH_OF_ADD_TO_LINKED_IDS__LINKDED_IDS_.push(created.id)
     }
     data._MEMBER_NAME_ = idsOfCreated
-    _SWITCH_OF_ADD_TO_LINKED_IDS__LINKDED_IDS_ = idsOfCreated
   }
+
+  if (data._PAYLOAD_NAME_FOR_ID_) {
+    if(data._MEMBER_NAME_ && data._MEMBER_NAME_.length > 0) data._MEMBER_NAME_.push(...data._PAYLOAD_NAME_FOR_ID_)
+    else data._MEMBER_NAME_ = data._PAYLOAD_NAME_FOR_ID_
+    _LINKDED_IDS_.push(...data._PAYLOAD_NAME_FOR_ID_)
+  }
+  
