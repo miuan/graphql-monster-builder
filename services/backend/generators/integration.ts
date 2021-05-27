d14cb2af1df93506172f66bb97878048a9188abd
Milan Medlik <milan.medlik@gmail.com>
2021-05-27 18:40:21 +0200

    generate test for ONE

--- services/backend/generators/integration.ts
+++ services/backend/generators/integration.ts
@@ -21,112 +21,6 @@ function wrapStringValue(member: SchemaModelMember, value: any){
     return value
 }
 
-function createTestAll(type: string, model: SchemaModel, role) {
-    const queryName = `all${model.modelName}`
-    let res = ''
-
-    let members = model.members.filter((m)=>!m.relation).map((m)=>m.name)
-    let relations = model.members.filter((m)=>m.relation).map((m)=>`${m.name}{id}`)
-    res += `const query = \`{query ${queryName} {
-        ${[...members, ...relations].join(',')}
-    }\`
-    
-    const res = await server.query({
-        query,
-        variables: { }
-      });
-
-    expect(res).not.toHaveProperty('data.${queryName}.token')
-    expect(res).not.toHaveProperty('data.${queryName}.refreshToken')
-    expect(res).toHaveProperty('errors')
-    `
-    return res;
-}
-
-
-function generateFields(model: SchemaModel, members: SchemaModelMember[], queryName: string, variables: any = {}, error=false) {
-    let res = ''
-    
-    
-    let not = error ? 'not.' : ''
-    res += error ? `expect(response).toHaveProperty('errors')` : `expect(response).not.toHaveProperty('errors')`
-
-
-    for(const member of members){
-
-        if(member.relation){
-            const variableName = member.relation.payloadNameForCreate
-            const relatedVariables = variables[variableName]
-
-            if(member.isArray){
-                if(relatedVariables){
-                    let arrayContaining = generateArrayContaining(relatedVariables, member, queryName);
-
-                    res +=`\nexpect(response.data.${queryName}.${member.name}).${not}toEqual(${arrayContaining})`
-                }
-                const variableNameId = member.relation.payloadNameForId
-                const relatedVariablesIds = variables[variableNameId]
-                if(relatedVariablesIds){
-                    const rvi = relatedVariablesIds.map((c)=>({id:c.replace(/=>/g, '').replace(/<=/g, '')}))
-                    let arrayContaining = generateArrayContaining(rvi, member, queryName);
-
-                    res +=`\nexpect(response.data.${queryName}.${member.name}).${not}toEqual(${arrayContaining})`
-                }
-
-                for(let i = 0; i < 3; i++) res += `\nexpect(response).${not}toHaveProperty('data.${queryName}.${member.name}.${i}.id')`
-            } else {
-                if(relatedVariables){
-            
-                }
-                res += `\nexpect(response).${not}toHaveProperty('data.${queryName}.${member.name}.id')`
-            }
-            
-
-        } else if(variables[member.name]){
-            let variable = variables[member.name]
-
-            variable = Array.isArray(variable) ? '[' + variable.reduce((p,c)=>`${p},${wrapStringValue(member,c)}`, '').substr(1) + ']' : wrapStringValue(member, variable)
-            res += `\nexpect(response).${not}toHaveProperty('data.${queryName}.${member.name}', ${variable})`
-        } else {
-            res += `\nexpect(response).${not}toHaveProperty('data.${queryName}.${member.name}')`
-        }
-    }
-    
-    return res
-}
-
-function generateArrayContaining(relatedVariables: any, member: SchemaModelMember, queryName: string) {
-    let arrayContaining = ``
-
-    for (const relatedVariable of relatedVariables) {
-        const additions = [];
-        const relatedMemberName = member.relation.relatedMember?.name;
-        if (relatedMemberName) {
-            additions.push(`${relatedMemberName}:expect.objectContaining({id:response.data.${queryName}.id})`);
-        }
-        const objectContaining = generateObjectContain(member.relation.relatedModel.members, relatedVariable, additions);
-        arrayContaining += `,\n\texpect.objectContaining({${objectContaining.substr(1)}})`;
-    }
-    arrayContaining = `expect.arrayContaining([${arrayContaining.substr(1)}])`;
-
-    return arrayContaining;
-}
-
-function generateObjectContain(members: SchemaModelMember[], relatedVariable: any, additions: any) {
-    let objectContaining = ``;
-    for (const relatedMember of members) {
-        const rmName = relatedMember.name;
-        const rmValue = relatedVariable[rmName];
-        if (rmValue) {
-            objectContaining += `,${rmName}: ${wrapStringValue(relatedMember, rmValue)}`;
-        }
-    }
-    for (const addition of additions){
-        objectContaining += `,${addition}`;
-    }
-    return objectContaining;
-}
-
 function generateVariables(model: SchemaModel, members: SchemaModelMember[], {skipRelation, config}:{skipRelation?:string, config?:any}={skipRelation:'', config: {}}) {
     let variables = {} as any
     
@@ -175,40 +69,7 @@ function generateRandomVariable(model: SchemaModel, member: SchemaModelMember) {
     return `${model.modelName}/${member.name}/${Math.random().toString(36).substring(5)}`;
 }
 
-function createTestCreate(model: SchemaModel, members: SchemaModelMember[], requiredOnly=false) {
-    const mutationName = `create${model.modelName}`
-    const mutationDesc = `Create${model.modelName}`
-    let res = ''
 
-    let output = generateOutputFromMembers(members)
-    let variableInputs = generateVariableInputsFromMembers(members)
-    let mutationInputs = members.filter((m)=>m.name !== 'id').map((m)=> m.relation? `${m.relation.payloadNameForCreate}: $${m.relation.payloadNameForCreate}, ${m.relation.payloadNameForId}: $${m.relation.payloadNameForId}` : `${m.name}: $${m.name}`).join(',')
-
-    const config = {
-        id: VARIABLE_CONFIG_SKIP
-    }
-    for(const memberWithRelation of members.filter((m)=>m.relation && m.relation.relatedMember)){
-        res += generateMongooseModelCreate(memberWithRelation, config);
-    }
-    
-    const variables = generateVariables(model, members, {config})
-
-
-    res += `const mutation = \`mutation ${mutationDesc}(${variableInputs}){
-        ${mutationName}(${mutationInputs}) {
-           ${output.join(',')}
-        }
-    }\`
-    
-    const response = await server.mutate({
-        mutation,
-        variables: ${JSON.stringify(variables, null, '\t').replace(/"=>/g, '').replace(/<="/g, '')}
-      }, res.data.login_v1.token);
-
-    ${generateFields(model, members, mutationName, variables)}
-    `
-    return res;
-}
 
 function generateMongooseModelCreate(member: SchemaModelMember, globalConfig: any) {
     const modelName = `model${member.modelName}`;
@@ -272,12 +133,166 @@ function generateOutputFromMembers(members: SchemaModelMember[]) {
     });
 }
 
+
+function generateArrayContaining(relatedVariables: any, member: SchemaModelMember, queryName: string) {
+    let arrayContaining = ``
+
+    for (const relatedVariable of relatedVariables) {
+        const additions = [];
+        const relatedMemberName = member.relation.relatedMember?.name;
+        if (relatedMemberName) {
+            additions.push(`${relatedMemberName}:expect.objectContaining({id:${queryName}Response.data.${queryName}.id})`);
+        }
+        const objectContaining = generateObjectContain(member.relation.relatedModel.members, relatedVariable, additions);
+        arrayContaining += `,\n\texpect.objectContaining({${objectContaining.substr(1)}})`;
+    }
+    arrayContaining = `expect.arrayContaining([${arrayContaining.substr(1)}])`;
+
+    return arrayContaining;
+}
+
+function generateObjectContain(members: SchemaModelMember[], relatedVariable: any, additions: any) {
+    let objectContaining = ``;
+    for (const relatedMember of members) {
+        const rmName = relatedMember.name;
+        const rmValue = relatedVariable[rmName];
+        if (rmValue) {
+            objectContaining += `,${rmName}: ${wrapStringValue(relatedMember, rmValue)}`;
+        }
+    }
+    for (const addition of additions){
+        objectContaining += `,${addition}`;
+    }
+    return objectContaining;
+}
+
+function generateExpects(model: SchemaModel, members: SchemaModelMember[], queryName: string, variables: any = {}, wrap = (member, variable) => variable, error=false) {
+    let res = ''
+
+    let not = error ? 'not.' : ''
+    res += error ? `expect(${queryName}Response).toHaveProperty('errors')` : `expect(${queryName}Response).not.toHaveProperty('errors')`
+
+    for(const member of members){
+
+        if(member.relation){
+            const variableName = member.relation.payloadNameForCreate
+            const relatedVariables = variables[variableName]
+
+            if(member.isArray){
+                // test is array is for One where is comming `createModel1Response.data.createModel1.model2` and thats generate milion of test lines
+                if(relatedVariables && Array.isArray(relatedVariables)){
+                    let arrayContaining = generateArrayContaining(relatedVariables, member, queryName);
+                    res +=`\nexpect(${queryName}Response.data.${queryName}.${member.name}).${not}toEqual(${arrayContaining})`
+                }
+                const variableNameId = member.relation.payloadNameForId
+                const relatedVariablesIds = variables[variableNameId]
+                if(relatedVariablesIds){
+                    const rvi = relatedVariablesIds.map((c)=>({id:c.replace(/=>/g, '').replace(/<=/g, '')}))
+                    let arrayContaining = generateArrayContaining(rvi, member, queryName);
+
+                    res +=`\nexpect(${queryName}Response.data.${queryName}.${member.name}).${not}toEqual(${arrayContaining})`
+                }
+
+                for(let i = 0; i < 3; i++) res += `\nexpect(${queryName}Response).${not}toHaveProperty('data.${queryName}.${member.name}.${i}.id')`
+            } else {
+                if(relatedVariables){
+            
+                }
+                res += `\nexpect(${queryName}Response).${not}toHaveProperty('data.${queryName}.${member.name}.id')`
+            }
+            
+
+        } else if(variables[member.name]){
+            let variable = variables[member.name]
+
+            variable = Array.isArray(variable) ? '[' + variable.reduce((p,c)=>`${p},${wrap(member,c)}`, '').substr(1) + ']' : wrap(member, variable)
+            res += `\nexpect(${queryName}Response).${not}toHaveProperty('data.${queryName}.${member.name}', ${variable})`
+        } else {
+            res += `\nexpect(${queryName}Response).${not}toHaveProperty('data.${queryName}.${member.name}')`
+        }
+    }
+    
+    return res
+}
+
+function createTestCreate(model: SchemaModel, members: SchemaModelMember[], requiredOnly=false) {
+    const mutationName = `create${model.modelName}`
+    const mutationDesc = `Create${model.modelName}`
+    let res = ''
+
+    let output = generateOutputFromMembers(members)
+    let variableInputs = generateVariableInputsFromMembers(members)
+    let mutationInputs = members.filter((m)=>m.name !== 'id').map((m)=> m.relation? `${m.relation.payloadNameForCreate}: $${m.relation.payloadNameForCreate}, ${m.relation.payloadNameForId}: $${m.relation.payloadNameForId}` : `${m.name}: $${m.name}`).join(',')
+
+    const config = {
+        id: VARIABLE_CONFIG_SKIP
+    }
+    for(const memberWithRelation of members.filter((m)=>m.relation && m.relation.relatedMember)){
+        res += generateMongooseModelCreate(memberWithRelation, config);
+    }
+    
+    const variables = generateVariables(model, members, {config})
+    
+    res += `const ${mutationName}Mutation = \`mutation ${mutationDesc}(${variableInputs}){
+        ${mutationName}(${mutationInputs}) {
+           ${output.join(',')}
+        }
+    }\`
+    
+    const ${mutationName}Response = await server.mutate({
+        mutation: ${mutationName}Mutation,
+        variables: ${JSON.stringify(variables, null, '\t').replace(/"=>/g, '').replace(/<="/g, '')}
+      }, res.data.login_v1.token);
+
+    ${generateExpects(model, members, mutationName, variables, (m,v)=>wrapStringValue(m,v))}
+    `
+    return res;
+}
+
+function createTestOne(model: SchemaModel, beforeMutation='create') {
+    const queryName = `one${model.modelName}`
+    const beforeMutationName = `${beforeMutation}${model.modelName}`
+    const beforeMutationResponseName = `${beforeMutationName}Response.data.${beforeMutationName}`
+    let res = '\n\n'
+    let output = generateOutputFromMembers(model.members)
+    const regEx = new RegExp(`data\\.${queryName}\\.`, 'g')
+    //const variables = generateVariables(model, members, {config})
+    let variables = model.members.reduce((accumulator, member)=>{
+        if(member.relation){
+            accumulator[member.name] = [
+                {id:`${beforeMutationResponseName}.${member.name}[0].id`},
+                {id:`${beforeMutationResponseName}.${member.name}[1].id`},
+                {id:`${beforeMutationResponseName}.${member.name}[2].id`}
+            ]
+        }
+        else accumulator[member.name] = `${beforeMutationResponseName}.${member.name}`
+        return accumulator
+    }, {})
+
+    res += `const ${queryName}Query = \`query ${model.modelName}($id: ID!){
+        ${model.modelName}(id: $id) {
+            ${output.join(',')}
+        }
+    }\`
+    
+    const ${queryName}Response = await server.query({
+        query: ${queryName}Query,
+        variables: { id: ${beforeMutationName}Response.data.${beforeMutationName}.id}
+      }, res.data.login_v1.token);
+
+      ${generateExpects(model, model.members, queryName, variables).replace(regEx, `data.${model.modelName}.`)}
+
+    `
+    return res;
+}
+
+
 function createAdminTest(structure: StructureBackend, model: SchemaModel) {
     // throw new Error('Function not implemented.');
     let res = ''
 
-    // res += createTestAll('all', model, 'admin')
     res += createTestCreate(model, model.members)
+    res += createTestOne(model)
 
     return res
 }
--- services/backend/integration-tests/login.integration.spec.ts
+++ services/backend/integration-tests/login.integration.spec.ts
@@ -78,71 +78,74 @@ describe('integration', ()=>{
 
             const modelModel2 = server.entry.models['model2']
 
-        const modelModel2Data = await Promise.all([
-            modelModel2.create({
-				"name": "Model2/name/v4319nzi",
-				"opt": "Model2/opt/pfj2q4lm",
-				"optFloat": 886542.9743051663,
-				"model1": "607bc7944481571f509470a2"
-}),
-            modelModel2.create({
-				"name": "Model2/name/pkb8wgo8",
-				"opt": "Model2/opt/8w76kodx",
-				"optFloat": 756007.503293699,
-				"model1": "607bc7944481571f509470a2"
-}),
-            modelModel2.create({
-				"name": "Model2/name/v3nf42nn",
-				"opt": "Model2/opt/zqq4ftw3",
-				"optFloat": 517728.7261187864,
-				"model1": "607bc7944481571f509470a2"
-})
-        ])
-            
-        const mutation = `mutation CreateModel1($name: String!,$opt: String,$optInt: Int,$optFloat: Float,$arrName: [String],$arrInt: [Int],$arrFloat: [Float],$optDateTime: DateTime,$model2: [InModel1MemberModel2AsModel2!],$model2Ids: [ID!]){
+            const modelModel2Data = await Promise.all([
+                modelModel2.create({
+                    "name": "Model2/name/pn79fc3g",
+                    "opt": "Model2/opt/065fcekg",
+                    "optFloat": 984062.5241212795,
+                    "model1": "607bc7944481571f509470a2",
+                    "id": "Model2/id/rfy7gim"
+    }),
+                modelModel2.create({
+                    "name": "Model2/name/2wok84c9",
+                    "opt": "Model2/opt/oa7w4nrd",
+                    "optFloat": 236149.58084816017,
+                    "model1": "607bc7944481571f509470a2",
+                    "id": "Model2/id/y1bz3k0a"
+    }),
+                modelModel2.create({
+                    "name": "Model2/name/e7221s5d",
+                    "opt": "Model2/opt/auo8t1wa",
+                    "optFloat": 43987.56298728812,
+                    "model1": "607bc7944481571f509470a2",
+                    "id": "Model2/id/4ntnlw8"
+    })
+            ])
+    
+        const createModel1Mutation = `mutation CreateModel1($name: String!,$opt: String,$optInt: Int,$optFloat: Float,$arrName: [String],$arrInt: [Int],$arrFloat: [Float],$optDateTime: DateTime,$model2: [InModel1MemberModel2AsModel2!],$model2Ids: [ID!]){
             createModel1(name: $name,opt: $opt,optInt: $optInt,optFloat: $optFloat,arrName: $arrName,arrInt: $arrInt,arrFloat: $arrFloat,optDateTime: $optDateTime,model2: $model2, model2Ids: $model2Ids) {
                name,opt,optInt,optFloat,arrName,arrInt,arrFloat,optDateTime,model2{name,opt,optFloat,model1{id},id},id
             }
         }`
         
-        const response = await server.mutate({
-            mutation,
+        const createModel1Response = await server.mutate({
+            mutation: createModel1Mutation,
             variables: {
-        "name": "Model1/name/37ob06bs",
-        "opt": "Model1/opt/54kt8x3",
-        "optInt": 52426,
-        "optFloat": 803894.1767597477,
+        "name": "Model1/name/g864fds4",
+        "opt": "Model1/opt/h8mh2hvi",
+        "optInt": 398661,
+        "optFloat": 941035.4038235695,
         "arrName": [
-            "Model1/arrName/v7u6c5x",
-            "Model1/arrName/cvxclq59",
-            "Model1/arrName/wi3dxb0p"
+            "Model1/arrName/7gbcv779",
+            "Model1/arrName/zvw8w4wo",
+            "Model1/arrName/otkz4x6o"
         ],
         "arrInt": [
-            178323,
-            961012,
-            759014
+            589617,
+            788864,
+            367384
         ],
         "arrFloat": [
-            494951.04467335274,
-            922622.0030313241,
-            308529.36044976034
+            354413.2473262602,
+            583756.2763030699,
+            175525.17500836527
         ],
-        "optDateTime": "2020-05-27T22:00:23.990Z",
+        "optDateTime": "2021-02-04T23:21:30.194Z",
         "model2": [
             {
-                "name": "Model2/name/qt9ynicd",
-                "opt": "Model2/opt/7h4q1bj",
-                "optFloat": 715329.6769870432
+                "name": "Model2/name/cnh5wx2r",
+                "opt": "Model2/opt/i09e3t8l",
+                "optFloat": 857354.143177198
             },
             {
-                "name": "Model2/name/dee7i3f",
-                "opt": "Model2/opt/rrd82w99",
-                "optFloat": 799453.7108380719
+                "name": "Model2/name/yhlzxwt9",
+                "opt": "Model2/opt/ua6hsc95",
+                "optFloat": 370633.45412272855
             },
             {
-                "name": "Model2/name/d25uxpo",
-                "opt": "Model2/opt/1nxcbn",
-                "optFloat": 59741.66343142051
+                "name": "Model2/name/k0jppsc",
+                "opt": "Model2/opt/iq4yr0g8",
+                "optFloat": 764942.5439849258
             }
         ],
         "model2Ids": [
@@ -153,28 +156,63 @@ describe('integration', ()=>{
     }
           }, res.data.login_v1.token);
     
-        expect(response).not.toHaveProperty('errors')
-    expect(response).toHaveProperty('data.createModel1.name', 'Model1/name/37ob06bs')
-    expect(response).toHaveProperty('data.createModel1.opt', 'Model1/opt/54kt8x3')
-    expect(response).toHaveProperty('data.createModel1.optInt', 52426)
-    expect(response).toHaveProperty('data.createModel1.optFloat', 803894.1767597477)
-    expect(response).toHaveProperty('data.createModel1.arrName', ['Model1/arrName/v7u6c5x','Model1/arrName/cvxclq59','Model1/arrName/wi3dxb0p'])
-    expect(response).toHaveProperty('data.createModel1.arrInt', [178323,961012,759014])
-    expect(response).toHaveProperty('data.createModel1.arrFloat', [494951.04467335274,922622.0030313241,308529.36044976034])
-    expect(response).toHaveProperty('data.createModel1.optDateTime', '2020-05-27T22:00:23.990Z')
-    expect(response.data.createModel1.model2).toEqual(expect.arrayContaining([
-        expect.objectContaining({name: 'Model2/name/qt9ynicd',opt: 'Model2/opt/7h4q1bj',optFloat: 715329.6769870432,model1:expect.objectContaining({id:response.data.createModel1.id})}),
-        expect.objectContaining({name: 'Model2/name/dee7i3f',opt: 'Model2/opt/rrd82w99',optFloat: 799453.7108380719,model1:expect.objectContaining({id:response.data.createModel1.id})}),
-        expect.objectContaining({name: 'Model2/name/d25uxpo',opt: 'Model2/opt/1nxcbn',optFloat: 59741.66343142051,model1:expect.objectContaining({id:response.data.createModel1.id})})]))
-    expect(response.data.createModel1.model2).toEqual(expect.arrayContaining([
-            expect.objectContaining({id: modelModel2Data[0].id,model1:expect.objectContaining({id:response.data.createModel1.id})}),
-            expect.objectContaining({id: modelModel2Data[1].id,model1:expect.objectContaining({id:response.data.createModel1.id})}),
-            expect.objectContaining({id: modelModel2Data[2].id,model1:expect.objectContaining({id:response.data.createModel1.id})})]))
-    expect(response).toHaveProperty('data.createModel1.model2.0.id')
-    expect(response).toHaveProperty('data.createModel1.model2.1.id')
-    expect(response).toHaveProperty('data.createModel1.model2.2.id')
-    expect(response).toHaveProperty('data.createModel1.id')
-            
+        expect(createModel1Response).not.toHaveProperty('errors')
+    expect(createModel1Response).toHaveProperty('data.createModel1.name', 'Model1/name/g864fds4')
+    expect(createModel1Response).toHaveProperty('data.createModel1.opt', 'Model1/opt/h8mh2hvi')
+    expect(createModel1Response).toHaveProperty('data.createModel1.optInt', 398661)
+    expect(createModel1Response).toHaveProperty('data.createModel1.optFloat', 941035.4038235695)
+    expect(createModel1Response).toHaveProperty('data.createModel1.arrName', ['Model1/arrName/7gbcv779','Model1/arrName/zvw8w4wo','Model1/arrName/otkz4x6o'])
+    expect(createModel1Response).toHaveProperty('data.createModel1.arrInt', [589617,788864,367384])
+    expect(createModel1Response).toHaveProperty('data.createModel1.arrFloat', [354413.2473262602,583756.2763030699,175525.17500836527])
+    expect(createModel1Response).toHaveProperty('data.createModel1.optDateTime', '2021-02-04T23:21:30.194Z')
+    expect(createModel1Response.data.createModel1.model2).toEqual(expect.arrayContaining([
+        expect.objectContaining({name: 'Model2/name/cnh5wx2r',opt: 'Model2/opt/i09e3t8l',optFloat: 857354.143177198,model1:expect.objectContaining({id:createModel1Response.data.createModel1.id})}),
+        expect.objectContaining({name: 'Model2/name/yhlzxwt9',opt: 'Model2/opt/ua6hsc95',optFloat: 370633.45412272855,model1:expect.objectContaining({id:createModel1Response.data.createModel1.id})}),
+        expect.objectContaining({name: 'Model2/name/k0jppsc',opt: 'Model2/opt/iq4yr0g8',optFloat: 764942.5439849258,model1:expect.objectContaining({id:createModel1Response.data.createModel1.id})})]))
+    expect(createModel1Response.data.createModel1.model2).toEqual(expect.arrayContaining([
+        expect.objectContaining({id: modelModel2Data[0].id,model1:expect.objectContaining({id:createModel1Response.data.createModel1.id})}),
+        expect.objectContaining({id: modelModel2Data[1].id,model1:expect.objectContaining({id:createModel1Response.data.createModel1.id})}),
+        expect.objectContaining({id: modelModel2Data[2].id,model1:expect.objectContaining({id:createModel1Response.data.createModel1.id})})]))
+    expect(createModel1Response).toHaveProperty('data.createModel1.model2.0.id')
+    expect(createModel1Response).toHaveProperty('data.createModel1.model2.1.id')
+    expect(createModel1Response).toHaveProperty('data.createModel1.model2.2.id')
+    expect(createModel1Response).toHaveProperty('data.createModel1.id')
+        
+    
+    const oneModel1Query = `query Model1($id: ID!){
+        Model1(id: $id) {
+            name,opt,optInt,optFloat,arrName,arrInt,arrFloat,optDateTime,model2{name,opt,optFloat,model1{id},id},id
+        }
+    }`
+    
+    const oneModel1Response = await server.query({
+        query: oneModel1Query,
+        variables: { id: createModel1Response.data.createModel1.id}
+      }, res.data.login_v1.token);
+
+
+      expect(oneModel1Response).not.toHaveProperty('errors')
+      expect(oneModel1Response).toHaveProperty('data.Model1.name', createModel1Response.data.createModel1.name)
+      expect(oneModel1Response).toHaveProperty('data.Model1.opt', createModel1Response.data.createModel1.opt)
+      expect(oneModel1Response).toHaveProperty('data.Model1.optInt', createModel1Response.data.createModel1.optInt)
+      expect(oneModel1Response).toHaveProperty('data.Model1.optFloat', createModel1Response.data.createModel1.optFloat)
+      expect(oneModel1Response).toHaveProperty('data.Model1.arrName', createModel1Response.data.createModel1.arrName)
+      expect(oneModel1Response).toHaveProperty('data.Model1.arrInt', createModel1Response.data.createModel1.arrInt)
+      expect(oneModel1Response).toHaveProperty('data.Model1.arrFloat', createModel1Response.data.createModel1.arrFloat)
+      expect(oneModel1Response).toHaveProperty('data.Model1.optDateTime', createModel1Response.data.createModel1.optDateTime)
+      expect(oneModel1Response.data.Model1.model2).toEqual(expect.arrayContaining([
+          expect.objectContaining({id: createModel1Response.data.createModel1.model2[0].id,model1:expect.objectContaining({id:oneModel1Response.data.Model1.id})}),
+          expect.objectContaining({id: createModel1Response.data.createModel1.model2[1].id,model1:expect.objectContaining({id:oneModel1Response.data.Model1.id})}),
+          expect.objectContaining({id: createModel1Response.data.createModel1.model2[2].id,model1:expect.objectContaining({id:oneModel1Response.data.Model1.id})})]))
+      expect(oneModel1Response).toHaveProperty('data.Model1.model2.0.id')
+      expect(oneModel1Response).toHaveProperty('data.Model1.model2.1.id')
+      expect(oneModel1Response).toHaveProperty('data.Model1.model2.2.id')
+      expect(oneModel1Response).toHaveProperty('data.Model1.id', createModel1Response.data.createModel1.id)
+      
+
+
+    
+    
         })
 
         it('register', async ()=>{
